484b681f96a892596879102d8c315ddff686b458
mrchu39 <mrchu39@yahoo.com>
2018-10-08 04:06:02 -0700

    Add files via upload

---
+++ exp1.py
@@ -0,0 +1,78 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from weighted_least_squares import *
+
+
+theta = np.array(range(7,102,5))*np.pi/180
+delta_theta = np.array(len(theta)*[0.5])
+I = np.array([374, 361, 350, 331, 308, 276, 242, 203, 189, 142, 112, 89, 62, 39, 22, 11, 3, 4, 2])
+delta_I = np.array(len(I)*[0.5])
+x = np.cos(theta)**2
+delta_x = np.abs(2*np.cos(theta)*np.sin(theta)*delta_I)
+
+m_unw = get_m(x,I,False)
+delta_m_unw = get_delta_m(x,I,False)
+b_unw = get_b(x,I,False)
+delta_b_unw = get_delta_b(x,I,False)
+delta_I_unw = get_delta_y(x,I,False)
+chi_sq_unw = chi_sq(x,I,False)
+
+
+delta_I_w = get_delta_y(x,I,True,delta_x)
+w = 1/delta_I_w
+m_w = get_m(x,I,w)
+delta_m_w = get_delta_m(x,I,w)
+b_w = get_b(x,I,w)
+delta_b_w = get_delta_b(x,I,w)
+chi_sq_w = chi_sq(x,I,True,delta_x)
+
+print "UNWEIGHTED LEAST SQUARES REGRESSION"
+print "I_0 =", m_unw, "+-", delta_m_unw
+print "I_bg =", b_unw, " +-", delta_b_unw
+print "delta I =", delta_I_unw
+print "chi squared =", chi_sq_unw
+print "-------------------------- \n"
+
+print "WEIGHTED LEAST SQUARES REGRESSION"
+print "I_0 =", m_w, " +-", delta_m_w
+print "I_bg =", b_w, " +-", delta_b_w
+print "delta I =", delta_I_w
+print "chi squared =", chi_sq_w
+print "-------------------------- \n"
+
+plt.figure(1)
+
+plt.errorbar(x, I, yerr=delta_I_unw, fmt='o')
+
+z = np.polyfit(x, I, 1)
+p = np.poly1d(z)
+plt.plot(x,p(x),"r--")
+plt.title(r'$\mathrm{Light \ Intensity \ vs. \ }\mathrm{cos}^2(\theta_{rel}) \ \mathrm{(Unweighted)}$')
+plt.xlabel(r'$\mathrm{cos}^2 (\theta_{rel})$')
+plt.ylabel('Light Intensity (lux)')
+
+plt.show()
+
+plt.figure(2)
+
+plt.errorbar(x, I, yerr=delta_I_w, fmt='o')
+
+line = np.array([m_w*x[0],m_w*x[len(x)-1]])
+plt.plot(np.array([x[0],x[len(x)-1]]),line,"r--")
+plt.title(r'$\mathrm{Light \ Intensity \ vs. \ }\mathrm{cos}^2(\theta_{rel}) \ \mathrm{(Weighted)}$')
+plt.xlabel(r'$\mathrm{cos}^2 (\theta_{rel})$')
+plt.ylabel('Light Intensity (lux)')
+
+plt.show()
+
+plt.figure(3)
+
+plt.plot(x, I, 'o')
+plt.plot(x,p(x),"r--",label='Unweighted')
+plt.plot(np.array([x[0],x[len(x)-1]]),line,"b--",label='Weighted')
+plt.title(r'$\mathrm{Light \ Intensity \ vs. \ }\mathrm{cos}^2(\theta_{rel}) \ \mathrm{(Both)}$')
+plt.xlabel(r'$\mathrm{cos}^2 (\theta_{rel})$')
+plt.ylabel('Light Intensity (lux)')
+plt.legend(loc='upper left')
+
+plt.show()
---
+++ exp3.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from weighted_least_squares import *
+
+ang = 90-np.array([10, 45, 67, 56])
+height = np.array([4.5, 3.5, 2.5, 2.5])
+height /= 10
+
+m = get_m(height, ang, False)
+b = get_b(height, ang, False)
+delta_y = np.array(len(ang)*[get_delta_y(height, ang, False)])
+delta_m = get_delta_m(height, ang, 1/delta_y)
+delta_b = get_delta_b(height, ang, 1/delta_y)
+
+plt.errorbar(height, ang, yerr=delta_y, fmt='o')
+z = np.polyfit(height, ang, 1)
+p = np.poly1d(z)
+plt.plot(height,p(height),"r--")
+plt.title(r'$\mathrm{Optical \ Axis \ Change \ vs. \ Height \ of \ Fluid}$')
+plt.xlabel('Height of Fluid (cm)')
+plt.ylabel('Optical Axis Change (degrees)')
+plt.show()
+
+print "m =", m, "+-", delta_m
+print "b =", b, "+-", delta_b
+print "r^2 =", get_r_sq(height,ang,True)
